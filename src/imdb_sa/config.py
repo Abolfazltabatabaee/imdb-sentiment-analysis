db61f88395b6a09b3d044c47c44a0d540cbf8b7b
Abolfazl Tabatabaee <abolfazltabtab0@gmail.com>
2026-02-02 16:11:20 +0330

    Update gitignore and finalize config/docs

--- src/imdb_sa/config.py
+++ src/imdb_sa/config.py
@@ -5,8 +5,8 @@ IMDB_URL = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 BASE_DIR = Path(__file__).resolve().parents[2]
 DATA_DIR = BASE_DIR / "data" / "raw"
 
-LIMIT_PER_CLASS = 2000
+LIMIT_PER_CLASS = None
 RANDOM_STATE = 42
 
-MAX_FEATURES = 10000
-NGRAM_RANGE = (1, 1)
+MAX_FEATURES = 20000
+NGRAM_RANGE = (1, 2)
